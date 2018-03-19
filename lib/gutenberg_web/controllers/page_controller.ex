85011338cb433648a512f9cfa3891e58f919671b
Brian Gracie <7625540+bgracie@users.noreply.github.com>
2018-03-19 13:52:05 -0400

    Swap out bootstrap for pure

--- lib/gutenberg_web/controllers/page_controller.ex
+++ lib/gutenberg_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule GutenbergWeb.PageController do
   use GutenbergWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    redirect(conn, to: "/books")
   end
 end
