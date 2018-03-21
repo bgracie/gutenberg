b60d0a3689a919cbf39e26c4b907563925a38dd5
Brian Gracie <7625540+bgracie@users.noreply.github.com>
2018-03-21 11:55:42 -0400

    Basic pagination

--- lib/gutenberg_web/controllers/book_controller.ex
+++ lib/gutenberg_web/controllers/book_controller.ex
@@ -1,12 +1,34 @@
 defmodule GutenbergWeb.BookController do
   use GutenbergWeb, :controller
 
+  import Ecto.Query, warn: false
   alias Gutenberg.Books
   alias Gutenberg.Books.Book
+  alias Gutenberg.Repo
 
-  def index(conn, _params) do
-    books = Books.list_books()
-    render(conn, "index.html", books: books)
+  def index(conn, params) do
+    results_per_page = 10
+
+    current_page = (params["page"] && String.to_integer(params["page"])) || 1
+    total_pages = (Repo.one(from b in Book, select: count(b.id)) / results_per_page)
+      |> Float.ceil
+      |> round()
+
+    books = (from b in Book,
+      select: [:id, :title],
+      offset: ^((current_page - 1) * results_per_page),
+      order_by: b.title,
+      limit: 10,
+      preload: [:authors]
+    ) |> Repo.all
+
+    render(
+      conn,
+      "index.html",
+      books: books,
+      current_page: current_page,
+      total_pages: total_pages
+    )
   end
 
   def new(conn, _params) do
