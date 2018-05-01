e826e0b3eb546cbe7a7b2e0baac4b32f9df187f6
Brian Gracie <7625540+bgracie@users.noreply.github.com>
2018-05-01 11:52:12 -0400

    Enable clicking on dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,9 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import setupPureDropdown from "./setup_pure_dropdown"
+
+setupPureDropdown(window, window.document);
 
 // Import local files
 //
