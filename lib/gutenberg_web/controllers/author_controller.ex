d34341836b20e9b4b64875b13a17db1b4b4ba60a
Brian Gracie <7625540+bgracie@users.noreply.github.com>
2018-05-07 13:27:02 -0400

    Refine styling

--- lib/gutenberg_web/controllers/author_controller.ex
+++ lib/gutenberg_web/controllers/author_controller.ex
@@ -1,11 +1,6 @@
 defmodule GutenbergWeb.AuthorController do
   use GutenbergWeb, :controller
 
-  def index(conn, _params) do
-    authors = Gutenberg.Books.Authors.list()
-    render(conn, "index.html", authors: authors)
-  end
-
   def show(conn, %{"id" => id}) do
     author = Gutenberg.Books.Authors.get!(id)
       |> Gutenberg.Repo.preload([:books])
